70ada579938dbf91554894d2fc32deb66714c668
Harrison Nou <harrison.nou@gmail.com>
2025-04-16 16:40:16 -0700

    Reduced background resource usage

--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -13,71 +13,56 @@ const Background: React.FC<BackgroundProps> = memo(({ children }) => {
   return (
     <div className="relative min-h-screen overflow-hidden bg-slate-900">
       <div 
-        className="absolute inset-0 overflow-hidden will-change-transform"
+        className="absolute inset-0 overflow-hidden"
         style={{ contain: 'strict' }}
         >
         {/* Original Blobs */}
         <div 
-          className="absolute top-0 left-0 w-[40rem] h-[40rem] -translate-x-1/2 -translate-y-1/2 rounded-full opacity-20 bg-blue will-change-transform"
+          className="absolute top-0 left-0 w-[40rem] h-[40rem] -translate-x-1/2 -translate-y-1/2 rounded-full opacity-20 bg-blue"
           style={{ 
             filter: 'blur(7rem)',
-            transform: 'translate3d(-50%, -50%, 0)'
           }}
         ></div>
         <div 
-          className="absolute top-0 right-0 w-[35rem] h-[35rem] translate-x-1/3 -translate-y-1/3 rounded-full opacity-20 bg-purple will-change-transform"
+          className="absolute top-0 right-0 w-[35rem] h-[35rem] translate-x-1/3 -translate-y-1/3 rounded-full opacity-20 bg-purple"
           style={{ 
             filter: 'blur(7rem)',
-            animationDelay: '-2s',
-            transform: 'translate3d(33%, -33%, 0)'
           }}
         ></div>
         <div 
-          className="absolute bottom-0 left-1/2 w-[30rem] h-[30rem] -translate-x-1/2 translate-y-1/4 rounded-full opacity-20 bg-pink animate-blob-shift will-change-transform"
+          className="absolute bottom-0 left-1/2 w-[30rem] h-[30rem] -translate-x-1/2 translate-y-1/4 rounded-full opacity-20 bg-pink"
           style={{ 
             filter: 'blur(7rem)',
-            animationDelay: '-5s',
-            transform: 'translate3d(-50%, 25%, 0)'
           }}
         ></div>
         <div 
-          className="absolute bottom-0 right-0 w-[45rem] h-[45rem] translate-x-1/3 translate-y-1/3 rounded-full opacity-20 bg-indigo will-change-transform"
+          className="absolute bottom-0 right-0 w-[45rem] h-[45rem] translate-x-1/3 translate-y-1/3 rounded-full opacity-20 bg-indigo"
           style={{ 
             filter: 'blur(7rem)',
-            animationDelay: '-7s',
-            transform: 'translate3d(33%, 33%, 0)'
           }}
         ></div>
         <div 
-          className="absolute top-1/4 left-1/4 w-[28rem] h-[28rem] rounded-full opacity-15 bg-cyan will-change-transform"
+          className="absolute top-1/4 left-1/4 w-[28rem] h-[28rem] rounded-full opacity-15 bg-cyan"
           style={{ 
             filter: 'blur(6rem)',
-            animationDelay: '-3s',
-            transform: 'translate3d(-25%, -25%, 0)'
           }}
         ></div>
         <div 
-          className="absolute top-1/2 right-1/4 w-[32rem] h-[32rem] rounded-full opacity-15 bg-emerald will-change-transform"
+          className="absolute top-1/2 right-1/4 w-[32rem] h-[32rem] rounded-full opacity-15 bg-emerald"
           style={{ 
             filter: 'blur(6.5rem)',
-            animationDelay: '-1s',
-            transform: 'translate3d(15%, -30%, 0)'
           }}
         ></div>
         <div 
-          className="absolute bottom-1/3 left-1/5 w-[36rem] h-[36rem] rounded-full opacity-15 bg-amber will-change-transform"
+          className="absolute bottom-1/3 left-1/5 w-[36rem] h-[36rem] rounded-full opacity-15 bg-amber"
           style={{ 
             filter: 'blur(7.5rem)',
-            animationDelay: '-4s',
-            transform: 'translate3d(-15%, 20%, 0)'
           }}
         ></div>
         <div 
-          className="absolute top-2/3 right-1/2 w-[25rem] h-[25rem] rounded-full opacity-15 bg-rose will-change-transform"
+          className="absolute top-2/3 right-1/2 w-[25rem] h-[25rem] rounded-full opacity-15 bg-rose"
           style={{ 
             filter: 'blur(5.5rem)',
-            animationDelay: '-6s',
-            transform: 'translate3d(45%, -15%, 0)'
           }}
         ></div>
       </div>

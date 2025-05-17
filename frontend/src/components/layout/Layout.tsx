b155411780fe9ef04efaa69c681dd4482f5b8ac4
Harrison Nou <harrison.nou@gmail.com>
2025-05-17 15:21:47 -0700

    QueueManager

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import QueueManager from "../playback/QueueManager";
 import PlaybackControls from "../playback/PlaybackControls";
 import Navbar from "../navbar/Navbar";
@@ -8,24 +9,31 @@ interface LayoutProps {
   children: React.ReactNode;
 }
 
-const Layout = ({children}:LayoutProps): JSX.Element => {
-const {showQueueManager} = useMusicPlayer();
+const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const { showQueueManager } = useMusicPlayer();
 
   return (
-    <div id='layout' className="flex flex-col min-h-screen">
-      <Navbar/>
+    <div id="layout" className="flex flex-col min-h-screen">
+      <Navbar />
       <Background>
-          <div id="Content" className="flex flex-row overflow-y-auto">
-            <div className="flex-grow">
+        <div id="Content" className="flex flex-row overflow-hidden relative flex-grow">
+          <div className={`flex-grow overflow-y-auto transition-all duration-300 ${showQueueManager ? 'pr-80 md:pr-96' : ''}`}>
             {children}
-            </div>
-            {showQueueManager && <QueueManager/>}
           </div>
+          
+          {/* QueueManager with Tailwind transitions */}
+          <div 
+            className={`fixed top-16 right-0 bottom-0 z-10 h-[calc(100vh-80px)] md:h-[calc(100vh-4rem)] transition-transform duration-300 ease-in-out transform ${
+              showQueueManager ? 'translate-x-0' : 'translate-x-full'
+            }`}
+          >
+            <QueueManager />
+          </div>
+        </div>
       </Background>
-      {<PlaybackControls/>}
-      
+      <PlaybackControls />
     </div>
-  )
-}
+  );
+};
 
 export default Layout;

439c4449a7007e60a5a857b3ad25512601dcd64e
Harrison Nou <harrison.nou@gmail.com>
2025-06-14 18:25:52 -0700

    Verbose port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 import app from "./app";
 import { connectDB } from "./config/db";
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 connectDB()
   .then(() => {
     console.log("Connected to Database.");
-    app.listen(port, () => console.log("App is running!"));
+    app.listen(port, () => console.log(`App is running on port ${port}!`));
   })
   .catch((err) => {
     console.error("Error:", err);
